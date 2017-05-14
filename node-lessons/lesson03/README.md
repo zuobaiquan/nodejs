56e66704d9a58a8dc1325f33c9c980421aa9e83d
左佰全 <zuobaiquan@ipzoe.com>
2017-05-14 18:18:09 +0800

    update

--- node-lessons/lesson03/README.md
+++ node-lessons/lesson03/README.md
@@ -19,7 +19,6 @@
     "href": "http://cnodejs.org/topic/54207e2efffeb6de3d61f68f"
   }
 ]
-
 ```
 
 ## 挑战
@@ -64,7 +63,7 @@ cheerio(https://github.com/cheeriojs/cheerio ) 大家可以理解成一个 Node.
 
 还记得我们怎么新建一个项目吗？
 
-1. 新建一个文件夹，进去之后 `npm init`
+1. 新建一个文件夹，进去之后 `npm init --yes` 
 2. 安装依赖 `npm install --save PACKAGE_NAME`
 3. 写应用逻辑
 
@@ -100,8 +99,6 @@ app.get('/', function (req, res, next) {
 OK，一个简单的爬虫就是这么简单。这里我们还没有利用到 Node.js 的异步并发特性。不过下两章内容都是关于异步控制的。
 
 记得好好看看 superagent 的 API，它把链式调用的风格玩到了极致。
-
-
 
 
 
--- node-lessons/lesson03/app.js
+++ node-lessons/lesson03/app.js
@@ -1,15 +1,23 @@
 var express = require('express');
+
+//Node.js 版的 jquery，用来从网页中以 css selector 取数据，使用方式跟 jquery 一样。
 var cheerio = require('cheerio');
+// superagent 是个 http 方面的库，可以发起 get 或 post 请求。
 var superagent = require('superagent');
 
 var app = express();
 
 app.get('/', function (req, res, next) {
+  // 用 superagent 去抓取 https://cnodejs.org/ 的内容
   superagent.get('https://cnodejs.org/')
     .end(function (err, sres) {
+      // 常规的错误处理
       if (err) {
         return next(err);
       }
+      // sres.text 里面存储着网页的 html 内容，将它传给 cheerio.load 之后
+      // 就可以得到一个实现了 jquery 接口的变量，我们习惯性地将它命名为 `$`
+      // 剩下就都是 jquery 的内容了
       var $ = cheerio.load(sres.text);
       var items = [];
       $('#topic_list .topic_title').each(function (idx, element) {
