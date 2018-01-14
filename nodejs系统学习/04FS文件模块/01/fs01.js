97e9f407902d61ebecdd6b868b777f7c4648d721
zuobaiquan <zuobaiquan@qq.com>
2018-01-14 11:17:51 +0800

    d

--- nodejs系统学习/04FS文件模块/01/fs01.js
+++ nodejs系统学习/04FS文件模块/01/fs01.js
@@ -38,111 +38,82 @@ var fs=require('fs');
   //callback      回调，传递异常参数err
 
 fs.mkdir('css',function(err){
- if(err){
-  console.log(err);
-  return false;
- }
- console.log('创建目录成功');
+   if(err){
+    console.log(err);
+    return false;
+   }
+   console.log('创建目录成功');
 })
 
-
-
-
-
-
-//3. fs.writeFile  创建写入文件
+//3. fs.writeFile  创建写入文件（如果文件存在 ，直接写入，如果文件有内容会覆盖，没有没有文件，会自动创建文件再追加）
 
 //filename      (String)            文件名称
 //data        (String | Buffer)    将要写入的内容，可以使字符串 或 buffer数据。
 //options        (Object)           option数组对象，包含：
- //· encoding   (string)            可选值，默认 ‘utf8′，当data使buffer时，该值应该为 ignored。
- //· mode         (Number)        文件读写权限，默认值 438
- //· flag            (String)            默认值 ‘w'
+//· encoding   (string)            可选值，默认 ‘utf8′，当data使buffer时，该值应该为 ignored。
+//· mode         (Number)        文件读写权限，默认值 438
+//· flag            (String)            默认值 ‘w'
 //callback {Function}  回调，传递一个异常参数err。
 
-
 //fs.writeFile('t.txt','你好nodejs 覆盖','utf8',function(err){
-//
 //   if(err){
 //    console.log(err);
-//
 //    return false;
 //   }
 //  console.log('写入成功');
 //})
-
-
-
 
 //4. fs.appendFile 追加文件
 //
 //fs.appendFile('t1.txt','这是写入的内容',function(err){
-//
 //   if(err){
 //    console.log(err);
-//
 //    return false;
 //   }
 //  console.log('写入成功');
 //})
 
-
-
 //fs.appendFile('t1.txt','这是写入的内容111\n',function(err){
-//
 // if(err){
 //  console.log(err);
-//
 //  return false;
 // }
 // console.log('写入成功');
 //})
 
 
-
-
 //5.fs.readFile 读取文件
-
-
+//
 //fs.readFile('t1.txt',function(err,data){
 //   if(err){
 //    console.log(err);
-//
 //    return false;
 //   }
 //   //console.log(data);
-//
 //   console.log(data.toString());
 //
 //})
 
 
-
 //6.fs.readdir读取目录  把目录下面的文件和文件夹都获取到。
 
 //fs.readdir('html',function(err,data){
-//
 //    if(err){
 //     console.log(err);
-//
 //     return false;
 //   }
 //   console.log(data);
-//
 //})
 
 
 //拿到一个文件夹下面的所有目录
 
-
 //7.fs.rename 重命名
 //1.改名  2.剪切文件
 
 //fs.rename('html/index.html','html/news.html',function(err){
-//
 //    if(err){
 //     console.log(err);
-//
 //     return false;
 //   }
 //   console.log('修改名字成功');
@@ -150,27 +121,19 @@ fs.mkdir('css',function(err){
 
 
 //fs.rename('html/css/basic.css','html/style.css',function(err){
-//
 //     if(err){
 //      console.log(err);
-//
 //      return false;
 //    }
 //    console.log('剪切成功');
 //})
 
 
-
-
-
-
-//
 //8. fs.rmdir  删除目录
 
 //fs.rmdir('t',function(err){
 //      if(err){
 //       console.log(err);
-//
 //       return false;
 //     }
 //    console.log('删除目录成功');
@@ -180,7 +143,6 @@ fs.mkdir('css',function(err){
 //fs.rmdir('index.txt',function(err){
 //      if(err){
 //       console.log(err);
-//
 //       return false;
 //     }
 //    console.log('删除目录成功');
@@ -189,14 +151,10 @@ fs.mkdir('css',function(err){
 
 //9. fs.unlink删除文件
 
-
-
-//
 // fs.unlink('index.txt',function(err){
-//
-//        if(err){
-//           console.log(err);
-//           return false;
-//      }
-//     console.log('删除文件成功');
+//    if(err){
+//      console.log(err);
+//      return false;
+//    }
+//    console.log('删除文件成功');
 // })
