7bc841b5982441b75696da57c5e6b6a4280dd65e
zuobaiquan <zuobaiquan@qq.com>
2018-01-17 22:45:19 +0800

    更新

--- nodejs系统学习/05利用HTTP模块URl模块Path模块Fs模块创建一个静态WEB服务器/nodejs静态web服务-下（demo）/model/getmimefromfile_bac.js
+++ nodejs系统学习/05利用HTTP模块URl模块Path模块Fs模块创建一个静态WEB服务器/nodejs静态web服务-下（demo）/model/getmimefromfile_bac.js
@@ -1,23 +1,17 @@
 exports.getMime=function(fs,extname){  /*获取后缀名的方法*/
-
   //.html
-    console.log('1');
-    fs.readFile('./mime.json',function(err,data){
-
-        if(err){
-            console.log('mime.json文件不存在');
-            return false;
-        }
-        //console.log(data.toString());
-
-        var Mimes=JSON.parse(data.toString());
-
-        //console.log(Mimes[extname]);
-        console.log('2');
-        return Mimes[extname] || 'text/html';
-
-    })
-
-    console.log('3');
-
+  console.log('1');
+  fs.readFile('./mime.json',function(err,data){
+      if(err){
+          console.log('mime.json文件不存在');
+          return false;
+      }
+      //console.log(data.toString());
+      var Mimes=JSON.parse(data.toString());
+      //console.log(Mimes[extname]);
+      console.log('2');
+      return Mimes[extname] || 'text/html';
+  })
+  //异步操作
+  console.log('3');
 }
