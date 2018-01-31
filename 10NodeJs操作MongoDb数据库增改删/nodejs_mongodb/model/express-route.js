4f3ee07503deff07bfc26be46ab542bb28461582
zuobaiquan <zuobaiquan@qq.com>
2018-01-31 22:18:47 +0800

    nodejs操作数据库

--- 10NodeJs操作MongoDb数据库增改删/nodejs_mongodb/model/express-route.js
+++ 10NodeJs操作MongoDb数据库增改删/nodejs_mongodb/model/express-route.js
@@ -14,64 +14,37 @@ function changeRes(res){
 
 //暴露的模块
 var Server=function(){
-
-
     var G=this;   /*全局变量*/
-
     //处理get和post请求
     this._get={};
-
     this._post={};
-
-
-
     var app=function(req,res){
-
-
         changeRes(res);
-
         //获取路由
         var pathname=url.parse(req.url).pathname;
         if(!pathname.endsWith('/')){
             pathname=pathname+'/';
         }
-
         //获取请求的方式 get  post
         var method=req.method.toLowerCase();
 
-
         if(G['_'+method][pathname]){
-
             if(method=='post'){ /*执行post请求*/
-
                 var postStr='';
                 req.on('data',function(chunk){
-
                     postStr+=chunk;
                 })
                 req.on('end',function(err,chunk) {
-
                     req.body=postStr;  /*表示拿到post的值*/
-
-
                     //G._post['dologin'](req,res)
-
                     G['_'+method][pathname](req,res); /*执行方法*/
-
                 })
-
-
-
             }else{ /*执行get请求*/
                 G['_'+method][pathname](req,res); /*执行方法*/
-
             }
-
         }else{
-
             res.end('no router');
         }
-
     }
 
     app.get=function(string,callback){
@@ -80,9 +53,7 @@ var Server=function(){
         }
         if(!string.startsWith('/')){
             string='/'+string;
-
         }
-
         //    /login/
         G._get[string]=callback;
 
@@ -94,18 +65,13 @@ var Server=function(){
         }
         if(!string.startsWith('/')){
             string='/'+string;
-
         }
         //    /login/
         G._post[string]=callback;
-
         //G._post['dologin']=function(req,res){
         //
         //}
     }
-
     return app;
-
 }
-
 module.exports=Server();
